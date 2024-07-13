fd271e6df26245f5d5ec346e7f295712d946b076
Andy-Vasseur <andyvasseurpro@outlook.com>
2024-07-13 15:10:31 +0200

    :wrench: add firestore.d.ts with *

---
+++ src/types/firestore.d.ts
@@ -0,0 +1,28 @@
+declare module "*"{
+
+    export interface Reserve {
+        id: string;
+        name: string;
+        description: string;
+        image: string;
+        link: string;
+        location: number;
+      }
+
+    export interface Animal {
+      id: string;
+      name: string;
+      silver: number;
+      gold: number;
+      diamond: number;
+      image: string;
+      max_weight: number;
+      level: number;
+      class: string;
+    }
+  
+    export const getAllReserves: (collectionName: string) => Promise<Reserve[]>;
+    export const getReserveByID: (collectionName: string, id: string) => Promise<Reserve>;
+    export const getAllAnimals: (collectionName: string) => Promise<Animal[]>;
+  }
+  
