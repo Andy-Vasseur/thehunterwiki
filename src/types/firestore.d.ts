bc99059de5dcff38e04f296ab24056561e4b8197
Andy-Vasseur <andyvasseurpro@outlook.com>
2024-07-13 15:17:41 +0200

    :wrench: remove strict linting for tsconfig.app.json

--- src/types/firestore.d.ts
+++ src/types/firestore.d.ts
@@ -1,28 +1,29 @@
-declare module "*"{
-
-    export interface Reserve {
-        id: string;
-        name: string;
-        description: string;
-        image: string;
-        link: string;
-        location: number;
-      }
-
-    export interface Animal {
-      id: string;
-      name: string;
-      silver: number;
-      gold: number;
-      diamond: number;
-      image: string;
-      max_weight: number;
-      level: number;
-      class: string;
-    }
-  
-    export const getAllReserves: (collectionName: string) => Promise<Reserve[]>;
-    export const getReserveByID: (collectionName: string, id: string) => Promise<Reserve>;
-    export const getAllAnimals: (collectionName: string) => Promise<Animal[]>;
+declare module "../../utils/firestore.js" {
+  export interface Reserve {
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    link: string;
+    location: number;
   }
-  
+
+  export interface Animal {
+    id: string;
+    name: string;
+    silver: number;
+    gold: number;
+    diamond: number;
+    image: string;
+    max_weight: number;
+    level: number;
+    class: string;
+  }
+
+  export const getAllReserves: (collectionName: string) => Promise<Reserve[]>;
+  export const getReserveByID: (
+    collectionName: string,
+    id: string
+  ) => Promise<Reserve>;
+  export const getAllAnimals: (collectionName: string) => Promise<Animal[]>;
+}
